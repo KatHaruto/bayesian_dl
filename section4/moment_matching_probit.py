9898a92379e67b5ce6fea49a4b38fba9798ede47
tr-author <katsushiro@g.ecc.u-tokyo.ac.jp>
2021-07-19 22:16:52 +0900

    plot result

--- section4/moment_matching_probit.py
+++ section4/moment_matching_probit.py
@@ -2,28 +2,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 from scipy.stats import multivariate_normal
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
+def confidence_ellipse(mu,cov, n_std=3.0, facecolor='none', **kwargs):
+
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0),
+        width=ell_radius_x * 2,
+        height=ell_radius_y * 2,
+        facecolor=facecolor,
+        **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = mu[0]
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = mu[1]
+
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ellipse
+
 
 def generate_data():
-    size = 100
-    mean_1 = np.array([4, 4])
+    pos_size = 100
+    neg_size = 100
+    mean_1 = np.array([1, 1])
     cov_1 = np.array([[1, 0], [0, 1]])
 
-    mean_2 = np.array([-1, -1])
+    mean_2 = np.array([3, -2])
     cov_2 = np.array([[1, 0], [0, 1]])
     # numpy を用いた生成
-    data_1 = np.random.multivariate_normal(mean_1, cov_1, size=size)
-    data_2 = np.random.multivariate_normal(mean_2, cov_2, size=size)
+    data_1 = np.random.multivariate_normal(mean_1, cov_1, size=neg_size)
+    data_2 = np.random.multivariate_normal(mean_2, cov_2, size=pos_size)
 
     X = np.concatenate([data_1,data_2])
-    Y = np.concatenate([-1*np.ones(100),np.ones(100)])
+    Y = np.concatenate([-1*np.ones(neg_size),np.ones(pos_size)])
     return X,Y
 class MomentMatchingProbit():
     def __init__(self):
-        self.liklihood = []
-        self.mus = []
-        self.vs = []
+        self.likelihood = []
         self.mu = np.array([0,0])
-        self.v = np.array([[100,0],[0,100]])
+        self.v = np.array([[10,0],[0,10]])
+        self.mus= []
+        self.vs= []
+        self.images = []
     def calc_Z(self,x,y):
         a = y*np.dot(x,self.mu) / np.sqrt(1+np.dot(self.v,x**2)+1e-6)
         return a,norm.cdf(a)
@@ -45,29 +82,46 @@ class MomentMatchingProbit():
         self.update_mu(x,y)
         self.update_v(x,y)
     def fit(self,X,Y):
+        self.evaluation(X,Y)
         for x,y in zip(X,Y):
             self.update_param(x,y)
             self.mus.append(self.mu)
             self.vs.append(self.v)
-            if self.calc_liklihood(X,Y) > -1e3:
-                self.liklihood.append(self.calc_liklihood(X,Y))
-    def calc_liklihood(self,X,Y):
+            self.evaluation(X,Y)
+            self.likelihood.append(self.calc_likelihood(X,Y))
+
+    def evaluation(self,X,Y):
+        color = np.concatenate([np.where(norm.cdf(np.dot(X[:100],self.mu)) < 0.5,'r','b'),
+                                np.where(norm.cdf(np.dot(X[100:],self.mu)) >= 0.5,'g','y')])
+        im1 = ax.scatter(X[:,0],X[:,1],color=color)
+        images.append([im1])
+        
+
+    def calc_likelihood(self,X,Y):
         return np.sum([np.log(norm.cdf(Y[i]*np.dot(self.mu,X[i]))+1e-6) for i in range(len(X))])
 
-    def plot(self):
-        plt.subplot(1,2,1)
-        plt.plot(list(range(len(self.liklihood))),self.liklihood)
-        plt.subplot(1,2,2)
-        #plt.plot(range(len(self.mus)),self.mus)
-        #plt.fill_between(range(len(self.vs)), self.mus+np.sqrt(self.vs), self.mus-np.sqrt(self.vs), color='lightgrey', label='predictive variance')
-        plt.show()
+
 
 X,Y = generate_data()
-
 pb = MomentMatchingProbit()
+fig = plt.figure()
+ax = fig.add_subplot(111)
+images = []
 pb.fit(X,Y)
-pb.plot()
-print(pb.mu,pb.v)
-print(sum(norm.cdf(np.dot(X[:100],pb.mu)) < 0.5))
-print(sum(norm.cdf(np.dot(X[100:],pb.mu)) > 0.5))
-plt.show()
+ani = animation.ArtistAnimation(fig, images)
+ani.save("./section4/result_moment_matching.gif",writer='pillow')
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+
+
+def update_anim(i):
+    ax.cla()
+    plt.xlim(-10,10)
+    plt.ylim(-10,10)
+    im = ax.scatter(pb.mus[i][0],pb.mus[i][1])
+    ax.add_patch(confidence_ellipse(pb.mus[i], pb.vs[i], edgecolor='red'))
+
+ani = FuncAnimation(fig, update_anim, frames=range(len(X)))
+
+ani.save("./section4/result_moment_matchin_w.gif",writer="pillow")
